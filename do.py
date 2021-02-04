f1aaf5aac2f1251eefa0d151ed7f10cbaf596e73
d3nd3 <2523452+d3nd3@users.noreply.github.com>
2021-02-04 15:28:32 +0000

    various improvements

--- do.py
+++ do.py
@@ -19,7 +19,6 @@ def search_for_closed_bracket(startindex,thelist):
             return x
     return None
 
-
 def grab_lump(lump_num,a_sof_map):
     bsp_header_size = 8
     lump_header_size = 8
@@ -51,6 +50,78 @@ def dump_textures(a_sof_map):
     #     print(f"texname : {tex}")
     return textures
 
+
+
+def grabField(inlines,classname,fieldname,wav):
+    found_intermission = -1
+    line_number = 0
+    for index,line in enumerate(inlines):
+        inlines[index].strip()
+        if line.startswith(f'"classname" "{classname}"'):
+            found_intermission = index
+            # print(f"debug:found {classname}")
+            break
+
+    if found_intermission != -1:
+        # found the above
+        start_line = search_for_open_bracket(found_intermission,inlines)
+        end_line = search_for_closed_bracket(found_intermission,inlines)
+        # error
+        if start_line is None or end_line is None:
+            print("error parsing entity table")
+            sys.exit(1)
+        targetname = ""
+        # search for targetname
+        for line in inlines[start_line+1:end_line]:
+            splitted_line = line.split('"')[3]
+            num_of_splits = len(splitted_line)
+            space_splitted = splitted_line.split()
+            len_space = len(space_splitted)
+            if line.startswith(f'"{fieldname}"'):
+                if len_space >= 1:
+                    targetname = space_splitted[0]
+                else:
+                    print("error parsing entity table")
+                    sys.exit(1)
+        if len(targetname) > 0:
+            # print(type(targetname))
+            if wav == WAVLESS:
+                if targetname.endswith(".wav"):
+                    print(f"WARNING: mapname:{inbsp} has non-functioning sound field {classname} @{fieldname}, remove .wav extension")
+                    return targetname
+                else:
+                    if '.' in targetname:
+                        targetname = targetname.split(".")[0]
+                    targetname += ".wav"
+                    return targetname
+            elif wav == OPTIONAL_WAV:
+                if targetname.endswith(".wav"):
+                    return targetname
+                else:
+                    if '.' in targetname:
+                        targetname = targetname.split(".")[0]
+                    targetname += ".wav"
+                    return targetname
+            elif wav == REQUIRES_WAV:
+                if targetname.endswith(".wav"):
+                    return targetname
+                else:
+                    print(f"WARNING: mapname:{inbsp} has non-functioning sound field {classname} @{fieldname}, append .wav extension")
+                    if '.' in targetname:
+                        targetname = targetname.split(".")[0]
+                    targetname += ".wav"
+                    return targetname
+
+                targetname.split()
+            return targetname
+    return None
+
+REQUIRES_WAV = 2
+OPTIONAL_WAV = 1
+WAVLESS = 0
+fieldFindSet = (("trigger_useable","targetname",REQUIRES_WAV),
+                ("target_speaker","noise",OPTIONAL_WAV)
+               )
 # get the exact classname data from entity text buffer
 def find_sounds(a_sof_map):
 
@@ -63,93 +134,43 @@ def find_sounds(a_sof_map):
     lines = stringfile.readlines()
     stringfile.close()
 
-    found_intermission = -1
-    line_number = 0
-    for index,line in enumerate(lines):
-        lines[index].strip()
-        if line.startswith('"classname" "trigger_speaker"'):
-            found_intermission = index
-            print("found trigger_speaker")
-            break
-
-    if found_intermission != -1:
-        # found the above
-        start_line = search_for_open_bracket(found_intermission,lines)
-        end_line = search_for_closed_bracket(found_intermission,lines)
-        # error
-        if start_line is None or end_line is None:
-            print("error parsing entity table")
-            sys.exit(1)
-        targetname = ""
-        # search for targetname
-        for line in lines[start_line+1:end_line]:
-            splitted_line = line.split('"')[3]
-            num_of_splits = len(splitted_line)
-            space_splitted = splitted_line.split()
-            len_space = len(space_splitted)
-            if line.startswith('"targetname"'):
-                if len_space >= 1:
-                    targetname = space_splitted[0]
-                else:
-                    print("error parsing entity table")
-                    sys.exit(1)
-        if len(targetname) > 0:
-            soundList+=targetname
-
-    found_intermission = -1
-    for index,line in enumerate(lines):
-        # print line
-        if line.startswith('"classname" "trigger_useable"'):
-            found_intermission = index
-            print("found trigger_useable")
-            break
-
-    if found_intermission != -1:
-        # found the above
-        start_line = search_for_open_bracket(found_intermission,lines)
-        end_line = search_for_closed_bracket(found_intermission,lines)
-        # error
-        if start_line is None or end_line is None:
-            print("error parsing entity table")
-            sys.exit(1)
-        targetname = ""
-        # search for targetname
-        for line in lines[start_line+1:end_line]:
-            splitted_line = line.split('"')[3]
-            num_of_splits = len(splitted_line)
-            space_splitted = splitted_line.split()
-            len_space = len(space_splitted)
-            if line.startswith('"targetname"'):
-                if len_space >= 1:
-                    targetname = space_splitted[0]
-                else:
-                    print("error parsing entity table")
-                    sys.exit(1)
-        if len(targetname) > 0:
-            soundList+=targetname
-
-    if len(soundList) == 0:
-        print("no sound data in this entlist")
+    soundList = []
+    for clss,fld,wavReq in fieldFindSet:
+        n = grabField(lines,clss,fld,wavReq)
+        if n is not None:
+            soundList.append(n)
     return soundList
 
 
-print(sys.argv[1])
+# print(sys.argv[1])
 inbsp = sys.argv[1]
 
 with open(inbsp,"rb") as sof_map:
     data = sof_map.read()
+    if struct.unpack_from('4s',data,0)[0] != b"IBSP":
+        print("map is corrupt")
+        sys.exit(1)
+
     # print( type(data))
+    print("Textures: ")
     all_textures = dump_textures(data)
-    print(all_textures)
+    for t in all_textures:
+        print(t)
+    
     if LOGGING:
         with open("tex.txt","w") as f:
             for each in all_textures:
                 f.write(each + "\n")
 
+    print()
+    print("Sounds: ")
     all_sounds = find_sounds(data)
-    print("sounds...")
-    print(all_sounds)
-    if LOGGING:
-        with open("sounds.txt","w") as f:
-            for snd in all_sounds:
-                f.write(snd + "\n")
+    if len(all_sounds) == 0:
+        print("no sound data in this entlist")
+    else:
+        for s in all_sounds:
+            print(s)
+        if LOGGING:
+            with open("sounds.txt","w") as f:
+                for snd in all_sounds:
+                    f.write(snd + "\n")
